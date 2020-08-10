1b39628ff4bfff5880d2c0c5c3e55d322dfcbccd
AritificialPhysics <sanskarsaraf10@gmail.com>
2020-08-11 00:02:08 +0530

    User choice handling added

--- phonebook.py
+++ phonebook.py
@@ -1,3 +1,23 @@
 """Integration and Front-End module"""
 
-# TODO
+import utils.user
+import utils.contacts
+
+def input_user_menu_choice():
+	"""Display user management options and returns user's input choice"""
+	print('1. Add user')
+	print('2. Delete user')
+	print('3: Select user')
+	choice = int(input('Input choice: '))
+
+	if choice < 1 or choice > 3:
+		raise Exception('Invalid choice')
+	else:
+		return choice
+
+if __name__ == "__main__":
+	user_menu_choice = input_user_menu_choice()
+
+	# TODO
+
+
