3304aaa18cdf6fe7154b3c678b2d64ca2fd72119
Sanskar <sanskarsaraf10@gmail.com>
2020-08-12 00:40:04 +0530

    Added function structure in both modules.

--- utils/contacts.py
+++ utils/contacts.py
@@ -1,3 +1,37 @@
-"""Module for contact management"""
+"""Module for contacts table management for each user."""
 
-# TODO
+# Name of the current user, initally None
+_username = None
+
+# Name of current table
+_tablename = "contacts_" + _username
+
+
+def show_all_contacts():
+	"""Shows all contacts in a user's contact table"""
+	pass
+
+
+def add_contact():
+	"""Adds a contact to the contacts table"""
+	pass
+
+
+def delete_contact(choice):
+	"""Deletes contact based on the user input from the contacts table"""
+	pass
+
+
+def search_contact():
+	"""Searches a contact in the current table"""
+	pass
+
+
+def import_csv():
+	"""Imports a CSV and stores the data into table"""
+	pass
+
+
+def export_csv():
+	"""Exports contents of table to a csv file. Input name from user"""
+	pass
--- utils/user.py
+++ utils/user.py
@@ -1,3 +1,51 @@
-"""Module for user management"""
+"""Module for user table management."""
 
-# TODO
+import contacts
+
+# TODO: Write __user_exists function
+def _user_exists():
+	pass
+
+def _input_credentials():
+	"""Inputs and returns username and password."""
+	username = input('Username: ')
+	password = input('Password: ')
+
+	return (username, password)
+
+
+def add_user():
+	"""Adds user to database if doesn't exist and create a contacts table for him.
+	Returns a string indicating the status
+	"""
+	username, password = _input_credentials()
+
+	if _user_exists(username, password):
+		return "The user already exists"
+	else:
+		# TODO: Add user to users table
+		# TODO: Create contacts table for user. Name: contacts_username
+		return "User successfully added"
+
+
+def remove_user():
+	"""Removes a user and associated contact table from the database"""
+	username, password = _input_credentials()
+
+	if _user_exists(username, password):
+		# TODO: Remove user from users table
+		# TODO: Remove users contacts table
+		return "User successfully removed"
+	else:
+		return "User not found"
+
+
+def select_user():
+	"""Selects a user for current contact operations"""
+	username, password = _input_credentials()
+
+	if _user_exists(username, password):
+		contacts._username = username
+		return "User successfully selected"
+	else:
+		return "User not found"
