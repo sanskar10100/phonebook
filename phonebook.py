1cf8e06bcc82afb4dd3467f670bc20f5203cbd67
Jack <kumaranuj1303@gmail.com>
2020-08-17 22:33:44 +0530

    Assigned EOFError to go back to previous menu

--- phonebook.py
+++ phonebook.py
@@ -11,7 +11,6 @@ from utils import user
 from utils import contacts
 from utils import helper
 
-
 def _input_user_menu_choice():
 	"""Displays user management options and returns user's input choice"""
 	print()
@@ -58,65 +57,70 @@ def _input_contact_menu_choice():
 def _user_management():
 	"""Processes user management menu input"""
 	# Redraw user management menu until a user is successfully selected
-	while True:
-		user_choice = _input_user_menu_choice()
-		if user_choice == 1:
-			if user.add_user() is True:
-				print('User successfully added')
+	try:
+		while True:
+			user_choice = _input_user_menu_choice()
+			if user_choice == 1:
+				if user.add_user() is True:
+					print('User successfully added')
+				else:
+					print('Error: user already exists')
+			elif user_choice == 2:
+				if user.remove_user() is True:
+					print('User successfully removed')
+				else:
+					print('Error: user does not exist')
+			elif user_choice == 3:
+				if user.select_user() is True:
+					print('User successfully selected')
+					break
+				else:
+					print('Error: user does not exist')
 			else:
-				print('Error: user already exists')
-		elif user_choice == 2:
-			if user.remove_user() is True:
-				print('User successfully removed')
-			else:
-				print('Error: user does not exist')
-		elif user_choice == 3:
-			if user.select_user() is True:
-				print('User successfully selected')
-				break
-			else:
-				print('Error: user does not exist')
-		else:
-			helper.trigger_exit()
+				helper.trigger_exit()
+	except EOFError:
+		_user_management()
 
 
 def _contacts_management():
 	"""Processes contact management menu input."""
 	# Redraw contact management menu until user exits.
-	while True:
-		print('Contact Management Menu')
-		contacts_choice = _input_contact_menu_choice()
-		if contacts_choice == 1:
-			contacts.show_all_contacts()
-		elif contacts_choice == 2:
-			if contacts.add_contact() is True:
-				print('Contact added successfully')
+	try:
+		while True:
+			print('Contact Management Menu')
+			contacts_choice = _input_contact_menu_choice()
+			if contacts_choice == 1:
+				contacts.show_all_contacts()
+			elif contacts_choice == 2:
+				if contacts.add_contact() is True:
+					print('Contact added successfully')
+				else:
+					print('Could not add contact')
+			elif contacts_choice == 3:
+				if contacts.delete_contact() is True:
+					print('Contact deleted successfully')
+				else:
+					print('Contact not found')
+			elif contacts_choice == 4:
+				if contacts.search_contact() is False:
+					print('Contact lookup failed')
+			elif contacts_choice == 5:
+				if contacts.import_csv() is True:
+					print('CSV import successful')
+				else:
+					print('CSV import failed')
+			elif contacts_choice == 6:
+				if contacts.export_csv() is True:
+					print('CSV export successful')
+				else:
+					print('CSV export failed')
+			elif contacts_choice == 7:
+				helper.clear_screen()
+				_user_management()
 			else:
-				print('Could not add contact')
-		elif contacts_choice == 3:
-			if contacts.delete_contact() is True:
-				print('Contact deleted successfully')
-			else:
-				print('Contact not found')
-		elif contacts_choice == 4:
-			if contacts.search_contact() is False:
-				print('Contact lookup failed')
-		elif contacts_choice == 5:
-			if contacts.import_csv() is True:
-				print('CSV import successful')
-			else:
-				print('CSV import failed')
-		elif contacts_choice == 6:
-			if contacts.export_csv() is True:
-				print('CSV export successful')
-			else:
-				print('CSV export failed')
-		elif contacts_choice == 7:
-			helper.clear_screen()
-			_user_management()
-		else:
-			helper.trigger_exit()
-
+				helper.trigger_exit()
+	except EOFError:
+		_contacts_management()
 
 if __name__ == "__main__":
 	try:
