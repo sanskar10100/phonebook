15452f6dd8174f2f1e9f33cc6cde7775ef85c5ad
AritificialPhysics <sanskar10100@gmail.com>
2020-09-17 01:22:24 +0530

    Restore files
    This reverts commit bef352970956ba7e9e40550e85fab4d2a9f4b651.

---
+++ phonebook.py
@@ -0,0 +1,151 @@
+"""phonebook is a simple contact management tool written in python.
+
+It hosts seperate phonebooks or contact lists for each user (that's right, we support multiple users)
+Offers user management options like addition, removal and selection.
+Also offers contact management option for each user, like addition, deletetion, searching etc
+It also supports import and export of most CSV files for contacts
+"""
+
+
+from utils import user
+from utils import contacts
+from utils import helper
+
+def _input_user_menu_choice():
+	"""Displays user management options and returns user's input choice"""
+	print()
+	print('--------------------')
+	print('User Management Menu')
+	print('--------------------')
+	print()
+	print('1. Add user')
+	print('2. Delete user')
+	print('3. Select user')
+	print('4. Exit')
+	
+	choice = int(input('Input choice: '))
+	helper.clear_screen()
+	if choice < 1 or choice > 4:
+		raise ValueError
+	else:
+		return choice
+
+
+
+def _input_contact_menu_choice():
+	"""Displays contact management options and returns user's input choice"""
+	choice = 0
+	print()
+	print('-----------------------')
+	print('Contact Management Menu')
+	print('-----------------------')
+	print()
+	print('1. Show all contacts')
+	print('2. Add contact')
+	print('3. Delete contact')
+	print('4. Search contact')
+	print('5. Modify contact')
+	print('6. Import CSV')
+	print('7. Export CSV')
+	print('8. Switch to user management mode')
+	print('9. Exit')
+
+	choice = int(input('Input choice: '))
+	helper.clear_screen()
+	if choice < 1 or choice > 9:
+		raise ValueError
+	else:
+		return choice
+
+
+def _user_management():
+	"""Processes user management menu input"""
+	# Redraw user management menu until a user is successfully selected
+	try:
+		while True:
+			user_choice = _input_user_menu_choice()
+			if user_choice == 1:
+				if user.add_user() is True:
+					print('User successfully added')
+				else:
+					print('Error: user already exists')
+			elif user_choice == 2:
+				if user.remove_user() is True:
+					print('User successfully removed')
+				else:
+					print('Error: user does not exist')
+			elif user_choice == 3:
+				if user.select_user() is True:
+					print('User successfully selected')
+					break
+				else:
+					print('Error: user does not exist')
+			else:
+				helper.trigger_exit()
+	except EOFError:
+		_user_management()
+	except ValueError:
+		print('Error: Invalid Input. Please try again!')
+		_user_management()
+
+
+def _contacts_management():
+	"""Processes contact management menu input."""
+	# Redraw contact management menu until user exits.
+	try:
+		while True:
+			contacts_choice = _input_contact_menu_choice()
+			if contacts_choice == 1:
+				contacts.show_all_contacts()
+			elif contacts_choice == 2:
+				if contacts.add_contact() is True:
+					print('Contact added successfully')
+				else:
+					print('Could not add contact')
+			elif contacts_choice == 3:
+				if contacts.delete_contact() is True:
+					print('Contact deleted successfully')
+				else:
+					print('Contact not found')
+			elif contacts_choice == 4:
+				if contacts.search_contact() is False:
+					print('Contact lookup failed')
+			elif contacts_choice == 5:
+				if contacts.modify_contact() is True:
+					print('Contact modified successfully')
+				else:
+					print('Unable to modify contact')
+			elif contacts_choice == 6:
+				if contacts.import_csv() is True:
+					print('CSV import successful')
+				else:
+					print('CSV import failed')
+			elif contacts_choice == 7:
+				if contacts.export_csv() is True:
+					print('CSV export successful')
+				else:
+					print('CSV export failed')
+			elif contacts_choice == 8:
+				_user_management()
+				helper.clear_screen()
+			else:
+				helper.trigger_exit()
+	except EOFError:
+		_contacts_management()
+	except ValueError:
+		print('Error: Invalid Input. Please try again!')
+		_contacts_management()
+
+
+if __name__ == "__main__":
+	try:
+		_user_management()
+		helper.clear_screen()
+	except KeyboardInterrupt:
+		helper.trigger_exit()
+	
+	try:
+		_contacts_management()
+		helper.clear_screen()
+	except KeyboardInterrupt:
+		helper.trigger_exit()
---
+++ utils/user.py
@@ -0,0 +1,174 @@
+"""Module for user table management."""
+
+from . import contacts
+from . import helper
+import hashlib
+import sqlite3
+import getpass
+import re
+
+
+# opening database and setting connection
+conn = sqlite3.connect('material.db')
+c = conn.cursor()
+
+
+def _user_exists(username):
+	"""Returns true if the username already exists in the users table."""
+	login = [username]
+	encrypted_credentials = _encryption(login)
+	# User table doesn't exist on first run, so silence the error from sqlite
+	# that the table doesn't exist
+	try:
+		sql = c.execute("SELECT username FROM users WHERE username = ?", encrypted_credentials)
+	except:
+		pass
+
+	row = c.fetchone()
+	if row is None:
+		return False
+	else:
+		return True
+
+
+def _user_auth(username, password):
+	"""Returns true if the username and password both exist in the database."""
+	login_credentials = [username, password]
+	encrypted_credentials = _encryption(login_credentials)
+
+	# If table lookup fails, most likely cause is that table doesn't exist, in which case
+	# simply returns false
+	try:
+		sql = c.execute('''SELECT * FROM users
+					WHERE username = ? AND password = ?''', encrypted_credentials)
+	except:
+		return False
+
+	record = c.fetchone()
+	if record is None:
+		return False
+	else:
+		return True
+
+
+def _input_credentials():
+	"""Inputs and returns username and password."""
+	username = input('Username: ')
+	password = getpass.getpass('Password: ')
+
+	return (username, password)
+
+
+def _print_credential_criteria():
+	"""Prints the username and password criteria on the screen."""
+	print('\nUsername specification: At least 4 character long. Can contain alphabets or numbers only')
+
+	print('\nPassword specifications:')
+	print('At least 8 characters')
+	print('At least one lowercase alphabet [a-z]')
+	print('At least one uppercase alphabet [A-Z]')
+	print('At least one digit [0-9]')
+	print('At least one special character [@, #, $, &, +, -, *, ?, ., :, /, ;]\n')
+
+
+def _verify_credential_criteria(username, password):
+	"""Verifies the credential criteria. Reprompt user if fails.
+
+	username: must be at least 4 alphanumeric characters long
+	password: must be at least 8 characters long. Further password criteria described below
+	"""
+	# Verifying username criteria
+	if len(username) < 4 or not username.isalnum():
+		print('Error: Username must be at least 4 alphanumeric characters long')
+		return False
+
+	flag = True
+	# Checking the password strength
+	if len(password) < 8:
+		flag = False
+	elif not re.search('[a-z]', password):
+		flag = False
+	elif not re.search('[A-Z]', password):
+		flag = False
+	elif not re.search('[0-9]', password):
+		flag = False
+	elif not re.search('[_:@#$&+-?.*;/]', password):
+		flag = False
+
+	if flag == False:
+		print('Error: Password did not match the specifications!')
+		return False
+
+
+def _encryption(login_details):
+	"""Return the login credential in sha256 encrypted format."""
+	encrypt_login_details = list()
+	for credential in login_details:
+		encrypt_login_details.append(hashlib.sha256(credential.encode()).hexdigest())
+	return tuple(encrypt_login_details)
+
+
+def add_user():
+	"""Adds user to database if doesn't exist and create a contacts table for him."""
+	_print_credential_criteria()
+	print('\nAdd user')
+	username, password = _input_credentials()
+	# Prompt user for username and password again if credential criteria doesn't match
+	if _verify_credential_criteria(username, password) is False:
+		add_user()
+
+	if _user_exists(username):
+		return False
+	else:
+		#Add user to users table
+		c.execute("""CREATE TABLE IF NOT EXISTS users( 
+			username VARCHAR(256) NOT NULL,
+			password VARCHAR(256) NOT NULL);
+		""")
+		encrypted_credentials = _encryption([username, password])
+		c.execute("INSERT INTO users VALUES (?,?);", encrypted_credentials)
+		conn.commit()
+
+		#Create contacts table for user. Name: contacts_username
+		#Scrubing the username.
+		tablename = helper.scrub('contacts_' + username)
+		c.execute(f"""CREATE TABLE {tablename} (
+			name VARCHAR(255) NOT NULL,
+			phno VARCHAR(20) NOT NULL,
+			email VARCHAR(255) NOT NULL);
+			""")
+		
+		conn.commit()
+
+		return True
+
+
+def remove_user():
+	"""Removes a user and associated contact table from the database."""
+	print('\nDelete user')
+	username, password = _input_credentials()
+
+	if _user_auth(username, password):
+		#Remove user from users table
+		encrypted_username = _encryption([username])
+		c.execute("DELETE FROM users WHERE username = ?", encrypted_username)
+		conn.commit()
+
+		#Remove users contacts table
+		tablename = helper.scrub('contacts_' + username)
+		c.execute(f"DROP TABLE {tablename} ")
+		return True
+	else:
+		return False
+
+
+def select_user():
+	"""Selects a user for current contact operations."""
+	print('\nSelect user')
+	username, password = _input_credentials()
+
+	if _user_auth(username, password):
+		contacts._set_tablename(username)
+		return True
+	else:
+		return False
